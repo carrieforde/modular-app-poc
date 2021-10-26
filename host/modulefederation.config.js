fb5dc90a7f994043efb1982bbd7333ce9f4f2d2b
Carrie Forde <cforde@lendage.com>
2021-10-26 13:07:23 -0700

    Adds craco & initial configs

---
+++ host/modulefederation.config.js
@@ -0,0 +1,21 @@
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  name: "host",
+  remotes: {
+    todo: "todo@http://localhost:3002/remoteEntry.js",
+  },
+  filename: "remoteEntry.js",
+  shared: {
+    ...deps,
+    react: {
+      singleton: true,
+      requiredVersion: deps["react"],
+    },
+    "react-dom": {
+      singleton: true,
+      requiredVersion: deps["react-dom"],
+    },
+  },
+};
+
---
+++ todo/modulefederation.config.js
@@ -0,0 +1,18 @@
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  name: "todo",
+  filename: "remoteEntry.js",
+  shared: {
+    ...deps,
+    react: {
+      singleton: true,
+      requiredVersion: deps["react"],
+    },
+    "react-dom": {
+      singleton: true,
+      requiredVersion: deps["react-dom"],
+    },
+  },
+};
+
